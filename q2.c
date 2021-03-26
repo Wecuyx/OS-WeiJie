49c91756bb7d7e4c9d2ed128b56398f61a90d9e1
Wecuyx <51123213+Wecuyx@users.noreply.github.com>
2021-03-26 18:28:07 +0800

    Update Q2

--- q2.c
+++ q2.c
@@ -12,11 +12,11 @@ int first_fit(int process[10]){
 
     for(int i = 0; i < 10; i++){
         for(int h = 0; h < 12; h++){
-            if(allocation[h] == 0 && process[i] <= holes[h]){
+            if(flag[i] == 0 && process[i] <= holes[h]){
                     allocation[h] = 1;
                     flag[i] = 1;
-                    holes[h] = holes[h] - process[i]; 
-                    break;
+                    holes[h] -= process[i];
+                    break; 
             }
         }
         //Checking after looping through the holes whether the process found a hole, if not return an error notification message 
@@ -24,6 +24,7 @@ int first_fit(int process[10]){
             printf("\nThe input Process with Size %d KB cannot fit into any remaining memory partition. It has to wait for other processes' completion");
         }
     }
+    printf("\nFirst Fit: ");
     for(int i = 0; i < 12; i++){
         printf("\n%d", holes[i]);
     }
@@ -31,7 +32,6 @@ int first_fit(int process[10]){
 }
 
 int best_fit(int process[10]){
-    //Not Finished
     //Hardcode values for the holes 
     int holes[12] = {160,350,650,80,410,50,720,905,570,130,260,830};
     //Allocation to check if the allocation is being done once found a suitable hole
@@ -39,26 +39,29 @@ int best_fit(int process[10]){
     //Flag to check if the hole is already taken
     int flag[10] = {0,0,0,0,0,0,0,0,0,0};
     int lowest = process[9];
-    int best;
     int temp;
     for(int i = 0; i < 10; i++){
+        int best = -1;
         for(int h = 0; h < 12; h++){
-            if(allocation[h] == 0){
-                temp = holes[h] - process[h];
-                    if(temp >= 0 && lowest > temp){
-                        lowest = temp;
-                        best = h;
-                        allocation[h] = 1;
-                        flag[i] = 1;
+                    if(holes[h] >= process[i]){
+                        if(best == -1){
+                            best = h;
+                        }
+                        else if(holes[best] >holes[h]){
+                            best = h;
+                        }
                     }
-            }
         }
-        holes[i] = holes[best] - process[i]; 
-        //Checking after looping through the holes whether the process found a hole, if not return an error notification message 
+        if(best != -1){
+            holes[best] -= process[i];
+            flag[i] = 1;
+        }
+        // Checking after looping through the holes whether the process found a hole, if not return an error notification message 
         if(flag[i] == 0){
             printf("\nThe input Process with Size %d KB cannot fit into any remaining memory partition. It has to wait for other processes' completion", process[i]);
         }
     }
+    printf("\nBest Fit: ");
     for(int i = 0; i < 12; i++){
         printf("\n%d", holes[i]);
     }
@@ -94,7 +97,7 @@ int main(){
     }
     first_fit(input);
     best_fit(input);
-    worst_fit();
+    // worst_fit();
 
 
 
